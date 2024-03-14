f95285fa0ff60f927b5d7797f19008e09a4065ff
igamenovoer <igamenover@gmail.com>
2024-03-14 18:41:40 +0800

    updated

--- initscripts/install-essentials.sh
+++ initscripts/install-essentials.sh
@@ -10,7 +10,7 @@ if [ "$WITH_ESSENTIAL_APPS" != "true" ]; then
   exit 0
 fi
 
-apt-get install sudo nano mc ne software-properties-common -y
+apt-get install sudo nano mc ne software-properties-common tmux -y
 apt-get install openssh-server git net-tools curl -y
 apt-get install cmake cmake-curses-gui pkg-config -y
 apt-get install python3 python3-pip -y
