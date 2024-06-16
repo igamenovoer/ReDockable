93b2b7323eaa56d4c30227377f64b68b91e7ffd9
igamenovoer <igamenovoer@gmail.com>
2024-06-17 02:03:33 +0800

    updated

--- initscripts/install-essentials.sh
+++ initscripts/install-essentials.sh
@@ -10,9 +10,38 @@ if [ "$WITH_ESSENTIAL_APPS" != "true" ]; then
   exit 0
 fi
 
-apt-get install sudo nano mc ne software-properties-common -y
-apt-get install openssh-server git net-tools curl tmux -y
-apt-get install cmake cmake-curses-gui pkg-config -y
-apt-get install python3 python3-pip -y
-apt-get install xauth x11-apps qimgv -y
-apt-get install ffmpeg -y
+apt update && apt-get install -y \
+    git \
+    python3-venv \
+    python3-pip \
+    build-essential
+
+# set pip source
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+apt update && apt install -y --no-install-recommends \
+        git \
+        curl \
+        vim \
+        tmux \
+        ncdu \
+        iotop \
+        bzip2 \
+        gosu \
+        magic-wormhole \
+        libglib2.0-0 \
+        libgl1-mesa-glx \
+        python3-venv \
+        python3-pip \
+        build-essential \
+        libopencv-dev \
+        libstdc++-10-dev
+  
+  apt-get clean && apt-get autoclean
+
+# apt-get install sudo nano mc ne software-properties-common -y
+# apt-get install openssh-server git net-tools curl tmux -y
+# apt-get install cmake cmake-curses-gui pkg-config -y
+# apt-get install python3 python3-pip -y
+# apt-get install xauth x11-apps qimgv -y
+# apt-get install ffmpeg -y
